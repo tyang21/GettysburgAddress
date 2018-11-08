0bf60412c6ba147ef8c296ca8b2604a96bf6e271
tyang21 <42978521+tyang21@users.noreply.github.com>
2018-11-08 14:37:46 -0500

    Add files via upload

--- src/GettysburgAddress.java
+++ src/GettysburgAddress.java
@@ -1,6 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
-
 public class GettysburgAddress 
 {
-	Scanner Gettysburg =  new Scanner(new File(gettyburg_address))
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		Scanner file = new Scanner(new File("Gettysburg Address.txt"));
+		ArrayList<String> words = new ArrayList<String>();
+		while(file.hasNext())
+		{
+			words.add(file.next());
+		}
+		
+		String longest;
+		int Average = 0;
+		for(String a: words)
+		{
+			if (a.length() > longest.length())
+			{
+				longest = a;
+			}
+		}
+		
+		
+	}
 }
